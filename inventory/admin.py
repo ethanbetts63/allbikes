45a21e2693b52fe25a91c79fe72613baf8f76fdf
Ethan Betts <you@example.com>
2026-01-03 10:31:46 +0100

    checkpoint

--- inventory/admin.py
+++ inventory/admin.py
@@ -8,9 +8,13 @@ class MotorcycleImageInline(admin.TabularInline):
 @admin.register(Motorcycle)
 class MotorcycleAdmin(admin.ModelAdmin):
     inlines = [MotorcycleImageInline]
-    list_display = ('make', 'model', 'year', 'condition', 'status', 'price', 'stock_number')
-    list_filter = ('condition', 'status', 'make')
+    list_display = ('make', 'model', 'year', 'display_conditions', 'status', 'price', 'stock_number')
+    list_filter = ('status', 'make', 'conditions')
     search_fields = ('make', 'model', 'stock_number')
+
+    def display_conditions(self, obj):
+        return ", ".join([condition.name for condition in obj.conditions.all()])
+    display_conditions.short_description = 'Conditions'
 
 @admin.register(MotorcycleCondition)
 class MotorcycleConditionAdmin(admin.ModelAdmin):
