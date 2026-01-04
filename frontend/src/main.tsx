607f273014b59137ced1bb4dbdca1429c1e33dfe
Ethan Betts <you@example.com>
2026-01-04 15:19:58 +0100

    checkpoint

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './context/AuthContext.tsx'
+import ScrollToTop from './components/ScrollToTop.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
+        <ScrollToTop />
         <App />
       </AuthProvider>
     </BrowserRouter>
