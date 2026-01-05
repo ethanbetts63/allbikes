bfb23de0d284062564882075de723d062c59c45e
Ethan Betts <you@example.com>
2026-01-05 18:03:47 +0100

    reset_django.ps1

--- reset_django.ps1
+++ reset_django.ps1
@@ -69,6 +69,7 @@ git commit -m "reset_django.ps1"
 git push
 
 python manage.py generate --terms
+python manage.py generate --brands
 python manage.py fix_site_domains
 python manage.py createsuperuser --username ethanbetts63@gmail.com --email ethanbetts63@gmail.com
 
