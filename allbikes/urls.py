abbc5cfe1c34141f197ad4c3012395746be756a0
Ethan Betts <you@example.com>
2026-01-06 14:02:34 +0100

    urls

--- allbikes/urls.py
+++ allbikes/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -28,6 +29,9 @@ urlpatterns = [
     # JWT Token Authentication Endpoints
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # Catch-all for the React frontend
+    re_path(r'^(?!api/|admin/|sitemap\.xml).*$', TemplateView.as_view(template_name="index.html")),
 ]
 
 if settings.DEBUG:
