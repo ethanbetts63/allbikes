d7dd094e5d0e0b9e06fb9094be0a7e594dbcacbb
Ethan Betts <you@example.com>
2026-01-08 13:36:55 +0100

    checkpoint

--- data_management/admin.py
+++ data_management/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import SiteSettings, TermsAndConditions
+from .models import TermsAndConditions
 
-admin.site.register(SiteSettings)
 admin.site.register(TermsAndConditions)
--- data_management/urls.py
+++ data_management/urls.py
@@ -1,22 +1,12 @@
 from django.urls import path
 from .views.user_profile_view import UserProfileView
-from .views.site_settings_view import SiteSettingsViewSet
 from .views.brand_list_view import BrandListView
 from .views.terms_and_conditions_view import LatestTermsAndConditionsView
 
 app_name = 'data_management'
 
-# Manually define the view for the singleton SiteSettings endpoint.
-# .as_view() maps HTTP methods to the ViewSet's actions.
-settings_view = SiteSettingsViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update'
-})
-
 urlpatterns = [
     path('me/', UserProfileView.as_view(), name='user-profile'),
-    path('settings/', settings_view, name='site-settings'),
     path('brands/', BrandListView.as_view(), name='brand-list'),
     path('terms/latest/', LatestTermsAndConditionsView.as_view(), name='latest-terms'),
 ]
