64b016dece4dfdeb80bbd18f00f70b2fe219b9c3
Ethan Betts <you@example.com>
2026-03-25 18:39:18 +0100

    checkpoint

--- inventory/management/commands/fetch_easycars.py
+++ inventory/management/commands/fetch_easycars.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 
 from ...utils.ftp_download import fetch_csv, fetch_images
 from ...utils.csv_reader import parse_feed
-from ...utils.db_importer import import_bikes
+from ...utils.db_importer import import_bikes, link_images, get_existing_image_paths
 
 
 class Command(BaseCommand):
@@ -10,11 +10,20 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write("--- Fetching CSV ---")
-        fetch_csv(self.stdout, self.stderr)
+        csv_text = fetch_csv(self.stderr)
+        if not csv_text:
+            return
 
-        self.stdout.write("--- Importing into database ---")
-        bikes = parse_feed()
+        self.stdout.write("--- Importing bikes ---")
+        bikes = parse_feed(csv_text)
         import_bikes(self.stdout, bikes)
 
         self.stdout.write("--- Fetching images ---")
-        fetch_images(self.stdout, self.stderr)
+        stock_numbers = {b["stock_number"] for b in bikes if b.get("stock_number")}
+        existing = get_existing_image_paths()
+        fetch_images(self.stdout, self.stderr, stock_numbers, existing)
+
+        self.stdout.write("--- Linking images ---")
+        link_images(self.stdout, bikes)
+
+        self.stdout.write(self.style.SUCCESS("Done."))
